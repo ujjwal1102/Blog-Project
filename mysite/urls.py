d09633d1f263a9793b73ee21f4ac12d96616bbff
ujjwal1102 <srivastavaaman1102@gmail.com>
2022-07-28 19:29:19 +0530

    first commit

---
+++ mysite/urls.py
@@ -0,0 +1,33 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+# from django.urls import path
+from django.conf.urls import include
+from django.contrib.auth import views
+from django.urls import re_path
+# from blog import views
+
+
+urlpatterns = [
+    re_path(r'^admin/',admin.site.urls),
+    # path('admin/', admin.site.urls),
+    re_path(r'',include('blog.urls')),
+    re_path(r'accounts/login/',views.LoginView.as_view(),name='login'),
+    re_path(r'accounts/logout/',views.LogoutView.as_view(),name='logout',kwargs={'next_page':'/'}),
+
+
+
+]
